6a9284c55c09626fffc8db4b09acd1c2504fd0e6
intisy (Finn Birich) <finn@birich.de>
2026-08-07 17:55:55 +0200

    chore: follow plugin-updater into plugins/

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         "@core-auth": fileURLToPath(new URL("./core-auth/dist", import.meta.url)),
         "@core-loader": fileURLToPath(new URL("./core-loader/dist", import.meta.url)),
         "@core-proxy": fileURLToPath(new URL("./core-proxy/dist", import.meta.url)),
-        "@plugin-updater": fileURLToPath(new URL("../../tools/plugin-updater/dist", import.meta.url)),
+        "@plugin-updater": fileURLToPath(new URL("../../plugins/plugin-updater/dist", import.meta.url)),
         "@config-ledger": fileURLToPath(new URL("../../plugins/config-ledger/dist", import.meta.url)),
       },
     },
